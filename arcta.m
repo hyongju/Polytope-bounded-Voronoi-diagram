2b4526c615d52c7f4700d8b685902d364ed9d5bd
Hyongju Park <Hyongju Park>
2016-11-21 13:47:46 -0500

    files updated for release

--- arcta.m
+++
@@ -1,7 +0,0 @@
-function out = arcta(th,delta)
-
-for i = 1:size(th,1)
-    num = dot([-sin(th(i)) cos(th(i))],delta(i,:));
-    den = dot([cos(th(i)) sin(th(i))],delta(i,:));
-    out(i,:) = atan2(num,den);
-end
--- f_exp.m
+++
@@ -1,3 +0,0 @@
-function f_val = f_exp(q,p,coef)
-
-f_val = exp(-coef*norm(q -p)^2);
